816bce5555b59b77c5c4ee0e7f8cc44db56afb6b
WXS <xwang152@stevens.edu>
2019-04-27 15:44:38 -0400

    Initial Commit

---
+++ Flea market/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flea market
+//
+//  Created by godlikeMini on 4/27/19.
+//  Copyright © 2019 SSW567. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
